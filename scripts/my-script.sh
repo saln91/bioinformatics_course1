7b17eee71168d7e763bf6e18465c49eeb2530794
saln91 <saloni.nachane@kcl.ac.uk>
2023-03-09 16:08:04 +0000

    not tested:updtaed script to make empty dirsand README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+mkdir -p analysis docs data
+#dd a README.md to each directory
+#the scripts already exist
+
+
+
+for my_directory in scripts analysis docs data; do
+	touch ${my_directory}/README.md
+	echo "# ${my_directory}" >> ${my_directory}/README.md
+done
